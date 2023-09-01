a70cbbd15bdcb38d3704ef472b291b2bfaa5a6ca
GustavoFFLima <frazao39@gmail.com>
2023-09-01 12:01:16 -0300

    desenvolvimento de crud

--- app.js
+++ app.js
@@ -99,6 +99,85 @@ app.patch('/clientes/:id_cliente', (req, res) => {
 })
 
 
+//fornecedores
+app.get('/fornecedores', (req, res) => {
+  connection.query(
+    'select * from cliente',
+    (err, results, fields) => {
+      if(err) console.log(err)
+      res.send(results)
+    }
+  );
+})
+
+app.get('/fornecedores/:id_cliente', (req, res) => {
+  var id_fornecedor  = req.params.id_fornecedor  
+  connection.query(
+    `select * from fornecedor where id_fornecedor = ${id_fornecedor }`,
+    (err, results, fields) => {
+      if(err) console.log(err)
+      res.send(results)
+    }
+  );
+})
+
+app.post('/fornecedores',  (req, res) => {
+  var razao = req.body.razao;
+  var cpf_cnpj = req.body.cpf_cnpj;
+  var contato = req.body.contato;
+  var logradouro = req.body.logradouro || null;
+  var cidade = req.body.cidade || null;
+  var uf = req.body.uf || null;
+  console.log(req.files)
+  var sql = `INSERT INTO fornecedor(razao, cpf_cnpj, contato, logradouro, cidade, uf) 
+    VALUES(
+      "${razao}", 
+      "${cpf_cnpj}", 
+      "${contato}", 
+      "${logradouro}", 
+      "${cidade}", 
+      "${uf}")`;
+
+   connection.query(sql, (erro, resultado) =>{
+      if(erro) res.send(erro)
+        res.send(resultado)
+    })
+  })
+
+app.patch('/fornecedores/:id_fornecedor', (req, res) => {
+  var id_fornecedor = req.params.id_fornecedor;
+  var razao = req.body.razao;
+  var cpf_cnpj = req.body.cpf_cnpj;
+  var contato = req.body.contato;
+  var logradouro = req.body.logradouro || null;
+  var cidade = req.body.cidade || null;
+  var uf = req.body.uf || null;
+  var sql = `
+    UPDATE fornecedor SET 
+      razao="${razao}", 
+      cpf_cnpj="${cpf_cnpj}", 
+      contato="${contato}", 
+      logradouro="${logradouro}", 
+      cidade="${cidade}", 
+      uf="${uf}" 
+    WHERE id_fornecedor=${id_fornecedor}`;
+
+   connection.query(sql, (erro, resultado) =>{
+      if(erro) res.send(erro)
+      res.send(resultado)
+   })
+})
+
+app.delete('/fornecedores/:id_fornecedor', (req, res) => {
+  var id_fornecedor  = req.params.id_fornecedor  
+  connection.query(
+    `DELETE FROM fornecedor WHERE id_fornecedor = ${id_fornecedor}`,
+    (err, results, fields) => {
+      if(err) console.log(err)
+      res.send(results)
+    }
+  );
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
